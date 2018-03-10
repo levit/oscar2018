8974069d369f9a1a28b68d271f25ec9a774eac73
Vinicius Rodrigues Nunes <vinny@voffice.com.br>
2018-03-10 07:46:28 -0300

    Configurar jackson para representar datas em formato ISO-8601

--- src/main/java/br/com/voffice/jwp2018/tf01/oscar/controllers/MoviesController.java
+++ src/main/java/br/com/voffice/jwp2018/tf01/oscar/controllers/MoviesController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
@@ -32,6 +33,7 @@ public class MoviesController extends HttpServlet {
 		List<Movie> movies = Arrays.asList(shapeOfWater, getOut, ladyBird, threeBillboards);
 		resp.setContentType("application/json");
 		ObjectMapper mapper = new ObjectMapper()
+				   .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 				   .registerModule(new ParameterNamesModule())
 				   .registerModule(new Jdk8Module())
 				   .registerModule(new JavaTimeModule());
