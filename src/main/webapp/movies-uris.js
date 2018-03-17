c085cdc2b5d25ccc8d531a1b643c4cc5e16b1270
Vinicius Rodrigues Nunes <vinny@voffice.com.br>
2018-03-16 22:34:00 -0300

    Centralizar uri base para uso no deployment

---
+++ src/main/webapp/movies-uris.js
@@ -0,0 +1,5 @@
+var baseUri = "http://localhost:8080/oscar";
+baseUri = "http://oscar2018.herokuapp.com/oscar";
+var moviesUri = baseUri+"/api/servlets/movies";
+var moviesKeyUri = baseUri+"/api/servlets/movies/key";
+var moviesBestPictureUri = baseUri+"/api/servlets/movies/oscar/nominees";
