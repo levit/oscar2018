54708324d8322e43dd435331ff259932e51755f2
Vinicius Rodrigues Nunes <vinny@voffice.com.br>
2018-03-17 11:41:26 -0300

    Adicionar exemplo de download

--- src/main/java/br/com/voffice/jwp2018/tf01/oscar/controllers/PostersController.java
+++ src/main/java/br/com/voffice/jwp2018/tf01/oscar/controllers/PostersController.java
@@ -1,17 +1,22 @@
 package br.com.voffice.jwp2018.tf01.oscar.controllers;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 @MultipartConfig
 @WebServlet("/api/servlets/movies/posters")
@@ -21,13 +26,25 @@ public class PostersController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String download = req.getParameter("download");
+		if (download != null) {
+			String path = getPosterPath();
+			File folder = new File(path);
+			File file = new File(folder, download);
+			byte[] contents = Files.readAllBytes(file.toPath());
+			ServletOutputStream outputStream = resp.getOutputStream();
+			BufferedOutputStream bos = new BufferedOutputStream(outputStream, 4096);
+			bos.write(contents);
+		} else {
 		List<String> posters = Files.list(Paths.get(getPosterPath())).filter(MoviesControllerFunctions.imageExtensionPredicate).map(p -> p.getFileName().toString()).sorted().collect(Collectors.toList());
 		resp.getWriter().write(MoviesControllerFunctions.toJSON(posters));
+		}
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getParts().stream().forEach(p -> MoviesControllerFunctions.savePart(getPosterPath(), p));
+		Collection<Part> parts = req.getParts();
+		parts.stream().forEach(p -> MoviesControllerFunctions.savePart(getPosterPath(), p));
 		resp.setStatus(HttpServletResponse.SC_ACCEPTED);
 		resp.sendRedirect(req.getRequestURI());
 	}
--- src/main/webapp/posters-uris.js
+++ src/main/webapp/posters-uris.js
@@ -1,2 +1,3 @@
 var POSTER_SERVER = "http://localhost:8081";
 var postersUri = "api/servlets/movies/posters";
+var postersDownloadUri = "api/servlets/movies/posters?download=";
